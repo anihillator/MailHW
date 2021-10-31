57fbe83019a4b9383f003c6ede70989211729c44
anihillator <anihillatorpol@gmail.com>
2021-10-31 12:36:36 +0300

    Added args and regex

--- pytest.py
+++ pytest.py
@@ -6,19 +6,31 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f","--filepath", metavar="", type=Path, help="Path for log file")
+parser.add_argument("-w","--word", metavar="", type=str, help="What to look for, either string or regex")
+if len (sys.argv)==1:
+    sys.exit("No arguments passed, -h for help. Execution aborted.")
 argsParsed = parser.parse_args()
+try:
+    re.compile(argsParsed.word)
+    regex_valid = True
+# re.I re.M
+# (^.*\.+log$)|(^.*\.+txt$)
+except re.error:
+    regex_valid = False
+print (regex_valid)
+if not argsParsed.filepath:
+    sys.exit("Please provide filepath. Execution aborted.")
 fPath=argsParsed.filepath
 if os.path.isfile(fPath):
     foundFiles=1
-    print("\nThat's a file!\n")
     with open(fPath) as file:
         lines = file.readlines()
-    for l in lines
+    for l in lines:
+        print (l)
         
 elif os.path.isdir(fPath):
-    foundFiles=1
-    print("\nThat's a directory!\n")
+    foundFiles=2
     dirFiles=os.listdir(fPath)
 else:
     foundFiles=0
-    print("Not found\n")
+    print("Files/path not found\n")
