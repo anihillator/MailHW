d180d88d6c0a0a3c50353687a7b21abde7a3d4be
anihillator <anihillatorpol@gmail.com>
2021-11-02 09:37:31 +0300

    Second attempt

--- pytest.py
+++ pytest.py
@@ -6,29 +6,27 @@ import os
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--filepath", metavar="", type=Path, help="Path the log file")
+parser.add_argument("-p", "--filepath", metavar="", type=Path, help="Path the log file")
 parser.add_argument("-w", "--word", metavar="", type=str, help="What to look for, either string or regex")
+parser.add_argument("-f", "--filemask", metavar="", type=str, help="Regex mask for filenames")
 if len(sys.argv) == 1:  # No args passed
     sys.exit("No arguments passed, -h for help. Execution aborted.")
 argsParsed = parser.parse_args()
 try:
-    re.compile(argsParsed.word)
-    regex_valid = True
+    txtMask = re.compile(argsParsed.filemask)
     regMask = re.compile(argsParsed.word)
+    regex_valid = True
 # re.I,re.M
 #
 except re.error:
     regex_valid = False
     sys.exit("Я не прикрутил поддержку плохого регекса, давайте заново.")
-if not argsParsed.filepath:
-    sys.exit("Please provide filepath. Execution aborted.")
 fPath = argsParsed.filepath
 
 if os.path.isfile(fPath):  # If given path is a file
     foundFiles = 1
     with open(fPath) as file:
         lines = file.readlines()
-    file.close()
     with open(r".\output.txt", "w") as outFile:  # Create output file
         print('Filename:{}'.format(fPath))
         for l in lines:
@@ -36,24 +34,22 @@ if os.path.isfile(fPath):  # If given path is a file
                 if regMask.search(l):  # Looking for matches in every line
                     #  print("Found a match:  ", l)  # Print each match into console
                     outFile.write(l)  # Print each match into file
-    outFile.close()
 # re.search
 elif os.path.isdir(fPath):
     foundFiles = 2
-    txtMask = re.compile(r"(.*\.+log)|(.*\.+txt)", re.IGNORECASE)  # Filetypes to look for, e.g. (^.*\.+log$)|(^.*\.+txt$)
+    #  txtMask = re.compile(r"(.*\.+log)|(.*\.+txt)", re.IGNORECASE)  # Filetypes to look for, e.g. (^.*\.+log$)|(^.*\.+txt$)
     dirFiles = os.listdir(fPath)  # All files in directory
     with open(r".\output.txt", "w") as outFile:
+        #  for i in tqdm(range(100), desc='Progress', colour="#03A062"):
+        #  for curFile in tqdm(dirFiles, desc='Progress'):  # Current file from the list
         for curFile in dirFiles:  # Current file from the list
             with open(os.path.join(fPath, curFile)) as cFile:  # Same as with single file, plus full path os.path.join(d, path)
                 if txtMask.search(curFile):
                     outFile.write('\nFilename:{}\n'.format(curFile))
                     lines = cFile.readlines()
-                    print('\nFilename:{}\n'.format(curFile))
-                    for i in tqdm(range(100), desc='Progress', colour="#03A062"):
-                        for l in lines:
-                            if regMask.search(l):
-                                outFile.write(l)
-    outFile.close()
+                    for l in tqdm(lines, desc='File {}'.format(curFile), colour="#03A062"):
+                        if regMask.search(l):
+                            outFile.write(l)
     print("\nDone!")
 else:
     foundFiles = 0
